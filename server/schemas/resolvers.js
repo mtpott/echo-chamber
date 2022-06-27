2abb936eaae444877ea4e152b4477e1657c15fe9
megan <mtpott23@gmail.com>
2022-06-27 17:00:13 -0400

    apolloServer connection established

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+    Query: {
+        helloWorld: () => {
+            return ':)';
+        }
+    }
+}; 
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -5,14 +5,34 @@ const { ApolloServer } = require('apollo-server-express');
 //middleware-->establish at a later date :)
 
 //IMPORT TYPEDEFS AND RESOLVERS ONCE ESTABLISHED
+const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001; 
 
 //ONCE TYPEDEFS AND RESOLVERS ARE CREATED, PASS IN AND CREATE NEW APOLLO SERVER
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server now running on port ${PORT} :)`);
+            console.log(`use graphql at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+}
+
+startApolloServer(typeDefs, resolvers);
