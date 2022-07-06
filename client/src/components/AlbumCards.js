5636b3fd43e32e5231ad4eb5a899f90d729de0e1
Jason Talbert <jason.talbert.jt@gmail.com>
2022-07-06 17:26:22 -0400

    semi-functional?

--- client/src/components/AlbumCards.js
+++ client/src/components/AlbumCards.js
@@ -1,134 +1,70 @@
-import React from "react";
-import { Container, Col, Row, Card, Button } from "react-bootstrap";
+import React, {useState, useEffect} from "react";
+// import { Container, Col, Row, Card, Button } from "react-bootstrap";
 
 
-const AlbumCards = () => {
+function AlbumCards() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-    return (
-       
-        <Container>
-  <Row>
-
-    <Col>
-
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-</Col>
-
-<Col>
-    
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    
-    </Col>
-
-    <Col>
-
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    
-    </Col>
-
-  </Row>
-
-  <Row>
-
-    <Col>
-    
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-    </Col>
-
-    <Col>
-    
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    
-    </Col>
-
-    <Col>
-    
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    
-    </Col>
-
-    <Col>
-    
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    
-    </Col>
-
-  </Row>
-</Container>
-          
-            );
+    const getItems = async () => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'a0f9c2e4cdmshbe352db2ac16675p113388jsne92dbdd69642',
+                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+            }
         };
-        
-        export default AlbumCards;
+
+      fetch("https://spotify23.p.rapidapi.com/search/?q=%25a%25&type=albums&offset=100&limit=10&numberOfTopResults=5", options)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result.data.data.albums);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }
+    
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        getItems();
+       
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <div className="card">
+      <img className="card-img-top" src={item.coverArt} alt="" />
+        <div className="card-body">
+          <h1 className="card-albumName">{item.name}</h1>
+          <h2 className="card-artist">{item.artists}</h2>
+        </div>
+    </div>
+            // <li key={item.id}>
+            //   {item.name} {item.price}
+            // </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+
+  export default AlbumCards;
